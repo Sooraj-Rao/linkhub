8045fcaa1b85549446279106faaa153d3267dd8c
Sooraj-Rao <soorajrao630@gmail.com>
2025-08-03 08:26:01 +0530

    prisma build try fix

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  try {
+    const count = await prisma.user.count();
+    return new Response(`User count: ${count}`, { status: 200 });
+  } catch (e) {
+    console.log(e)
+    return new Response(`Error: ${(e as Error).message}`, { status: 500 });
+  }
+}
